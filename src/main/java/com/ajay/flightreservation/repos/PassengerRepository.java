de85d67449a3647e324d01a8d6de9e96992339c2
ajaytvijay96 <42099782+ajaytvijay96@users.noreply.github.com>
2021-03-26 09:56:58 +0530

    Delete src/main/java/com/ajay/flightreservation directory

--- src/main/java/com/ajay/flightreservation/repos/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ajay.flightreservation.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ajay.flightreservation.entities.Passenger;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-
-}
--- src/main/java/com/ajay/flightreservation/services/ReservationService.java
+++
@@ -1,9 +0,0 @@
-package com.ajay.flightreservation.services;
-
-import com.ajay.flightreservation.dto.ReservationRequest;
-import com.ajay.flightreservation.entities.Reservation;
-
-public interface ReservationService {
-
-	public Reservation bookFlight(ReservationRequest request);
-}
